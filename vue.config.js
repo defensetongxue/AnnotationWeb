5f7d6e33d86ad62e1f27ceb69578dbe266fd5561
defense <1900013009@pku.edu.cn>
2023-04-09 14:24:11 +0800

    bug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+// vue.config.js
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/AnnotationWeb/'
-      : '/'
+      : '/',
+    configureWebpack: {
+      entry: {
+        app: './app.js',
+      },
+    },
   }
+  
